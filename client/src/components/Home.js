144ae6983309526fa6c1b60eb37e399f6930741f
Imran Sayed <imranhsayed@gmail.com>
2020-01-05 19:50:10 +0530

    Add product query

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,7 @@ import { useQuery } from "@apollo/react-hooks";
 import { gql } from "apollo-boost";
 import renderHTML from 'react-render-html';
 
-const postQuery = gql`
+const productQuery = gql`
 {
   products(first: 50) {
     nodes {
@@ -51,9 +51,9 @@ const postQuery = gql`
 
 
 export const Home = () => {
-	const { loading, error, data } = useQuery( postQuery );
+	const { loading, error, data } = useQuery( productQuery );
 
-	console.warn( 'hey', data );
+	console.warn( 'products', data );
 
 	if ( loading ) {
 		return <p>Loading...</p>
@@ -63,15 +63,4 @@ export const Home = () => {
 	}
 
 	return <div>Hey</div>;
-
-	return data.posts.edges.map( item => (
-		(
-			<div key={ item.node.id } className="card border-primary m-5" style={{ maxWidth: '20rem' }}>
-				<div className="card-header">{ item.node.title }</div>
-				<div className="card-body">
-					<div className="card-text">{ renderHTML( item.node.content ) }</div>
-				</div>
-			</div>
-		)
-	) )
 };
